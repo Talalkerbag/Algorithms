a27e7af42c2e4ed552ac5125712098f703804454
Talal Kerbag <kerbagtalal94@gmail.com>
2020-01-15 19:02:44 -0500

    Quadrilateral Max Area

--- com/williamfiset/algorithms/geometry/QuadrilateralMaxArea.java
+++ com/williamfiset/algorithms/geometry/QuadrilateralMaxArea.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+/**
+ * Java program to find maximum are of a quadrilateral
+ *
+ * @author Talal Kerbag, kerbagtalal94@gmail.com
+ */
+package com.williamfiset.algorithms.geometry;
 
 public class QuadrilateralMaxArea {
+    static double maxArea(double a, double b, double c, double d){
+        //Calculating the semi-perimeter of the given Quadrilatera
+        double semiperimeter = (a + b + c + d) / 2;
+
+        //Applying Brahmagupta's formula to get maximum area of quadrilateral
+        return Math.sqrt((semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c) * (semiperimeter - d));
+
+    }
+
+    //Test
+    public static void main(String [] args){
+        double a = 1, b = 2, c = 1, d = 2;
+        System.out.println("Max area of the quadrilateral: " + maxArea(a,b,c,d));
+    }
 }
